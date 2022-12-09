bce0dadbaa4302d4e246ba82031658bcba67c07c
KRIT NAMBUTT <toyota026265@gmail.com>
2022-12-09 15:09:21 +0700

    try to add more example codes & try add BottomNavigationBar Menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -10,15 +12,309 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+        title: 'Startup Name Generator',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+          ),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        home: const _WidgetBottomNavigationBar());
+  }
+}
+
+class _WidgetBottomNavigationBar extends StatefulWidget {
+  const _WidgetBottomNavigationBar({super.key});
+
+  @override
+  State<_WidgetBottomNavigationBar> createState() =>
+      WidgetBottomNavigationBarState();
+}
+
+class WidgetBottomNavigationBarState extends State<_WidgetBottomNavigationBar> {
+  int _currentIndex = 0;
+  List<Widget> body = const [
+    Icon(Icons.home),
+    Icon(Icons.menu),
+    Icon(Icons.person)
+  ];
+
+  void _onItemTapped(int newIndex) {
+    setState(() {
+      _currentIndex = newIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Center(
+        child: body[_currentIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: 'Menu', icon: Icon(Icons.menu)),
+          BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // NEW from here ...
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions',
+            ),
+          ],
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: /*1*/ (context, i) {
+            if (i.isOdd) return const Divider(); /*2*/
+
+            final index = i ~/ 2; /*3*/
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+            }
+
+            final alreadySaved = _saved.contains(_suggestions[index]);
+
+            return ListTile(
+              title: Text(
+                _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
+              trailing: Icon(
+                alreadySaved ? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved ? Colors.red : null,
+                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+              ),
+              onTap: () {
+                setState(() {
+                  if (alreadySaved) {
+                    _saved.remove(_suggestions[index]);
+                  } else {
+                    _saved.add(_suggestions[index]);
+                  }
+                }); // to here.
+              },
+            );
+          },
+        ));
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  bool showText = true;
+  bool toggleState = true;
+  Timer? t2;
+
+  void toggleBlinkState() {
+    setState(() {
+      toggleState = !toggleState;
+    });
+    if (!toggleState) {
+      t2 = Timer.periodic(const Duration(milliseconds: 1000), (t) {
+        toggleShowText();
+      });
+    } else {
+      t2?.cancel();
+    }
+  }
+
+  void toggleShowText() {
+    setState(() {
+      showText = !showText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            if (showText)
+              const Text(
+                'This execution will be done before you can blink.',
+              ),
+            Padding(
+              padding: const EdgeInsets.only(top: 70.0),
+              child: ElevatedButton(
+                onPressed: toggleBlinkState,
+                child: toggleState
+                    ? const Text('Blink')
+                    : const Text('Stop Blinking'),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class MyWidgetTextEditing extends StatelessWidget {
+  MyWidgetTextEditing({super.key});
+
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: Column(children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: 'Type something',
+              labelText: 'Text Field',
+            ),
+          ),
+          ElevatedButton(
+            child: const Text('Submit'),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text('Alert'),
+                      content: Text('You typed ${_controller.text}'),
+                    );
+                  });
+            },
+          ),
+        ]));
+  }
+}
+
+class MyFormStatefulWidget extends StatefulWidget {
+  const MyFormStatefulWidget({super.key});
+
+  @override
+  State<MyFormStatefulWidget> createState() => _MyFormStatefulWidgetState();
+}
+
+class _MyFormStatefulWidgetState extends State<MyFormStatefulWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter your email',
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate will return true if the form is valid, or false if
+                    // the form is invalid.
+                    if (_formKey.currentState!.validate()) {
+                      // Process data.
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
