54796190169523b8cd6d0c581770b57e7b56d9db
KRIT NAMBUTT <toyota026265@gmail.com>
2022-12-16 01:18:33 +0700

    grouping LastOrderFoodContent & remove incorrect icon on header LastOrderFoodContent

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -77,49 +77,31 @@ class LastOrderFoodContent extends StatelessWidget {
                             fontFamily: 'Prompt',
                             color: const Color(0xFF3D3D3D)),
                       ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      SvgPicture.asset(
-                        'assets/icons/right-arrow-fill.svg',
-                        width: 20,
-                        height: 20,
-                        color: const Color(0x66808080),
-                        fit: BoxFit.scaleDown,
-                      )
                     ],
                   ),
                   const SizedBox(
                     height: 10,
                   ),
-                  const RecentShopFoodContent(),
+                  SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: Row(
+                              children: List.generate(
+                                  recent_shop_food.length,
+                                  (index) => ShopCard(
+                                        imageSrc:
+                                            recent_shop_food[index].imageSrc,
+                                        shopName:
+                                            recent_shop_food[index].shopName,
+                                        distance:
+                                            recent_shop_food[index].distance,
+                                        press: recent_shop_food[index].press,
+                                      ))))),
                   const SizedBox(
                     height: 30,
                   ),
                 ])));
-  }
-}
-
-class RecentShopFoodContent extends StatelessWidget {
-  const RecentShopFoodContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Row(
-                children: List.generate(
-                    recent_shop_food.length,
-                    (index) => ShopCard(
-                          imageSrc: recent_shop_food[index].imageSrc,
-                          shopName: recent_shop_food[index].shopName,
-                          distance: recent_shop_food[index].distance,
-                          press: recent_shop_food[index].press,
-                        )))));
   }
 }
 
