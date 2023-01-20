c2ae140be650fcc69461c922efcaddbe99493bb3
KRIT NAMBUTT <toyota026265@gmail.com>
2023-01-20 21:44:50 +0700

    refactor ApiProvider

--- lib/api/api_provider.dart
+++ lib/api/api_provider.dart
@@ -1,17 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-var options = BaseOptions(
-  baseUrl: '${dotenv.get("HOST_URL")}/api',
-  headers: {'Content-Type': 'application/json; charset=utf-8'},
-  connectTimeout: 5000,
-  receiveTimeout: 3000,
-);
-
-final Dio dio = Dio(options);
-
 class ApiProvider {
   ApiProvider();
+
+  final Dio dio = Dio(BaseOptions(
+    baseUrl: '${dotenv.get("HOST_URL")}/api',
+    connectTimeout: 5000,
+    receiveTimeout: 3000,
+    headers: {'Content-Type': 'application/json; charset=utf-8'},
+  ));
 
   Future<Response> getLastFoodOrder() async {
     final url = '/shop_food/last-order-food';
